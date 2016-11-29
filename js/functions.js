6893ca6eae4f49ec54fbf609a10486110e5167b9
Caio Le Petit <caio.lepetit@gmail.com>
2016-11-29 20:11:49 -0200

    Ajuste do js e do botao de aumentar e reduzir

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,19 @@
 var $z = jQuery.noConflict();
 $z(document).ready(function(){
 	$z("#aumentar-fonte-carousel").click(function () {
-		var size = $z(".carousel-caption h3").css('font-size');
+		var size = $z("body h3").css('font-size');
 
 		size = size.replace('px', '');
 		size = parseInt(size) + 2;
 
-		$z(".carousel-caption h3").animate({'font-size' : size + 'px'});
+		$z("body h3").animate({'font-size' : size + 'px'});
 
-		var size = $z(".carousel-caption p").css('font-size');
+		var size = $z("body p").css('font-size');
 
 		size = size.replace('px', '');
 		size = parseInt(size) + 2;
 
-		$z(".carousel-caption p").animate({'font-size' : size + 'px'});
+		$z("body p").animate({'font-size' : size + 'px'});
 
 		return false;
 	});
@@ -34,77 +34,5 @@ $z(document).ready(function(){
 	$z(".carousel-caption p").animate({'font-size' : size + 'px'});
 
 	return false;
-	});
-
-	$z("#aumentar-fonte-produtos").click(function () {
-		var size = $z(".fonte-produto h3").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".fonte-produto h3").animate({'font-size' : size + 'px'});
-
-		var size = $z(".fonte-produto p").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".fonte-produto p").animate({'font-size' : size + 'px'});
-
-		return false;
-	});
-
-	$z("#diminuir-fonte-produtos").click(function () {
-		var size = $z(".fonte-produto h3").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) - 2;
-
-		$z(".fonte-produto h3").animate({'font-size' : size + 'px'});
-
-		var size = $z(".fonte-produto p").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) - 2;
-
-		$z(".fonte-produto p").animate({'font-size' : size + 'px'});
-
-		return false;
-	});
-
-	$z("#aumentar-fonte-sobre").click(function () {
-		var size = $z(".carousel-caption h3").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".carousel-caption h3").animate({'font-size' : size + 'px'});
-
-		var size = $z(".carousel-caption p").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".carousel-caption p").animate({'font-size' : size + 'px'});
-
-		return false;
-	});
-
-	$z("#aumentar-fonte-contato").click(function () {
-		var size = $z(".carousel-caption h3").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".carousel-caption h3").animate({'font-size' : size + 'px'});
-
-		var size = $z(".carousel-caption p").css('font-size');
-
-		size = size.replace('px', '');
-		size = parseInt(size) + 2;
-
-		$z(".carousel-caption p").animate({'font-size' : size + 'px'});
-
-		return false;
 	});
 });
